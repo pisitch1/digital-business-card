5f52b73e6380b7963f93973bcc3d5920ddc326bd
pisitch1 <pchartbanchachai@salesforce.com>
2023-11-20 16:00:43 -0800

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.seattleteeth.com",
   integrations: [
     tailwind(),
     react(),
@@ -14,5 +15,6 @@ export default defineConfig({
         forward: ["dataLayer.push"],
       },
     }),
+    sitemap(),
   ],
 });
